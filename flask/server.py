524a728b2f97c320bd21c1be30d5bfff7411c3e1
w_nanase <watanabe.nanase.nalgo.intern@gmail.com>
2019-08-23 11:38:12 +0900

    flask

--- flask/server.py
+++ flask/server.py
@@ -3,6 +3,7 @@ import random
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 #from werkzeug import secure_filename
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -10,37 +11,20 @@ ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg', 'JPG'])
 UPLOAD_FOLDER = './images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-# http://127.0.0.1:5000をルートとして、("")の中でアクセスポイント指定
-# @app.route("hoge")などで指定すると、http://127.0.0.1:5000/hogeでの動作を記述できる。
-@app.after_request
-def add_header(response):
-    # response.cache_control.no_store = True
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '-1'
-    return response
-
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET','POST'])
 def result():
  if request.method == 'POST':
         img_file = request.files['image']
-        if img_file and allowed_file(img_file.filename):
-            #filename = secure_filename(img_file.filename)
+        if img_file:
             filename="result.jpg"
             img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            return render_template('result.html', filename=filename)
+            return  render_template('result.html')
         else:
             return ''' <p>許可されていない拡張子です</p> '''
  else:
