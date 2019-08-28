76f93c16fbf7d8c6b32bbea6ce26a1273b08db6e
hysrtr331 <hayashi.ryotaro.nalgo.intern@gmail.com>
2019-08-28 12:20:12 +0900

    ディレクトリの移動とコメントアウト変更

---
+++ flow/Flickr_API.py
@@ -0,0 +1,50 @@
+"""
+以下に示すようなディレクトリを用意し、 python Flickr_API.py 検索名　というコマンドを実行すると
+検索名フォルダにFlickrから取得した画像を保存するプログラム。コマンドの検索名と作るフォルダ名は一致させる。
+
+ディレクトリ構造
+　　｜
+    |ーー検索名
+　　｜－－Flickr_API.py
+"""
+
+from flickrapi import FlickrAPI
+from urllib.request import urlretrieve
+from pprint import pprint
+import os, time, sys
+
+# API キーの情報
+#APIキーはFlickrで取得
+key = "272cfaeacd5d72e4ad8608b1f47158ea" #APIキー
+secret = "2f1ebd1d205f1de0" #secretキー
+
+wait_time = 1
+
+animalname = sys.argv[1]
+
+# 画像を保存するディレクトリを指定
+#自身の前のディレクトリにあるファイルを参照
+savedir = "./" + animalname
+
+flickr = FlickrAPI(key, secret, format='parsed-json')
+result = flickr.photos.search(
+    text = animalname,
+    per_page = 500, #取得するデータ件数(500がMAX)
+    media = 'photos',
+    sort = 'relevance',
+    safe_search = 1,
+    extras = 'url_q, licence'
+)
+
+# 結果を表示(URL)
+photos = result['photos']
+pprint(photos)
+
+#画像を保存
+for photo in photos['photo']:
+    url_q = photo['url_q']
+    filepath = savedir + '/' + photo['id'] + '.jpg'
+
+    if os.path.exists(filepath): continue
+    urlretrieve(url_q, filepath)
+    time.sleep(wait_time)
